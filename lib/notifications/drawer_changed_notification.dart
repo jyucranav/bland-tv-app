2fbe72bfe7f70efd64e7911d223a62216d10e2ee
Johann Yucra <johannyucra92@gmail.com>
2025-01-10 16:02:59 -0300

    changed default channel

--- lib/notifications/drawer_changed_notification.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class DrawerChangedNotification extends Notification {
-  final bool isOpen;
-
-  DrawerChangedNotification(this.isOpen);
-}
--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -4,5 +4,5 @@ class Globals {
   static String tvLocal = 'assets/json/canales.json';
 
   static List<Channel> channels = [];
-  static int currentChannel = 0;
+  static int currentChannel = 75;
 }
